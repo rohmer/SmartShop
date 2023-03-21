b1faba04e8fa33bd49278acb88c03d48ff3308c3
rohmer <rohmer@users.noreply.github.com>
2023-03-21 15:30:30 -0400

    Update sigar.sh

---
+++ Components/build-scripts/sigar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bdir=$(pwd)
+if [ ! -d /tmp/sigar.build ]; then
+	mkdir /tmp/sigar.build 
+fi
+cd /tmp/sigar.build
+cp -R $bdir/sigar-master/* 
+
+bash ./autogen.sh
+bash ./configure
+make -j4
+make install
