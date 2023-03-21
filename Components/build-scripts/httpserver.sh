ce2a99c05eb486e8780d332f001ddd3f100edfdb
rohmer <rohmer@users.noreply.github.com>
2023-03-21 14:13:52 -0400

    Update httpserver.sh

--- Components/build-scripts/httpserver.sh
+++ Components/build-scripts/httpserver.sh
@@ -8,11 +8,11 @@ cd /tmp/httpserver.build
 cp $bdir/WebServer/libhttpserver-master/* .
 cp -R $bdir/WebServer/libhttpserver-master/m4 .
 cp -R $bdir/WebServer/libhttpserver-master/*.am .
-cp $bdir/WebServer/libhttpserver-master/libhttpserver.pc.in .
-cp $bdir/WebServer/libhttpserver-master/doc/Makefile.in .
-cp $bdir/WebServer/libhttpserver-master/src/Makefile.in .
-cp $bdir/WebServer/libhttpserver-master/test/Makefile.in .
-cp $bdir/WebServer/libhttpserver-master/examples/Makefile.in .
+cp -r $bdir/WebServer/libhttpserver-master/libhttpserver.pc.in .
+cp -r $bdir/WebServer/libhttpserver-master/doc/Makefile.in ./doc
+cp -r $bdir/WebServer/libhttpserver-master/src/Makefile.in ./src
+cp -r $bdir/WebServer/libhttpserver-master/test/Makefile.in ./test
+cp -r $bdir/WebServer/libhttpserver-master/examples/Makefile.in ./examples
 
 /bin/bash $bdir/WebServer/libhttpserver-master/bootstrap
 
