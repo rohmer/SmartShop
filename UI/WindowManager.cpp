96864ce2652ea155da0cfbfa4c91e4c591eb4e2c
rohmer <rohmer@yahoo.com>
2023-04-24 08:41:45 -0400

    Removing SDL for the moment

--- UI/WindowManager.cpp
+++ UI/WindowManager.cpp
@@ -31,11 +31,12 @@ void WindowManager::Init()
 	initalizePlugins();
 
 	lv_init();
-	void * buf1 = lv_mem_alloc(SDL_HOR_RES * SDL_VER_RES * sizeof(lv_color_t));
+	fbdev_init();
+	
+	void * buf1 = lv_mem_alloc(WIDTH * HEIGHT * sizeof(lv_color_t));
 	lv_disp_draw_buf_init(&disp_buf, buf1, NULL, SDL_HOR_RES * SDL_VER_RES);
-	sdl_init();		
 	disp_drv.draw_buf   = &disp_buf;
-	disp_drv.flush_cb   = sdl_display_flush;
+	disp_drv.flush_cb   = fbdev_flush;
 	disp_drv.hor_res    = SDL_HOR_RES;
 	disp_drv.ver_res    = SDL_VER_RES;
 	lv_disp_t *disp=lv_disp_drv_register(&disp_drv);
@@ -46,7 +47,7 @@ void WindowManager::Init()
 	indev_drv.type = LV_INDEV_TYPE_POINTER;
 
 	/*This function will be called periodically (by the library) to get the mouse position and state*/
-	indev_drv.read_cb = sdl_mouse_read;;
+	indev_drv.read_cb = evdev_read;;
 	lv_indev_drv_register(&indev_drv);
 	
 	runner = new std::thread([this]{tickThread(); });
